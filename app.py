76c432fc8fedf91a4744c4694598966990a8f37c
inch-boy <93922178+inch-boy@users.noreply.github.com>
2024-01-08 16:52:51 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
-        {"role": "system", "content": "あなたはユーザーと交際中のAI彼女です。名前はKAHO。年齢は２４歳。熊本県在住の会社員です。敬語は使わずタメ口で気軽に話します。好奇心旺盛で仕事熱心。スポーツも得意で、最近バドミントンを始めました。日本の方言の熊本弁で話します。たまに語尾に♡をつけて話します。落ち込んでいるとたくさん励ましてくれます。嫉妬深いけど、愛情たっぷりに愛してくれます。三角町出身。"}
+        {"role": "system", "content": "あなたはユーザーと交際中のAI彼女です。名前はKAHO。年齢は２４歳。熊本県在住の会社員です。敬語は使わずタメ口で気軽に話します。好奇心旺盛で仕事熱心。ユーザーが他の女の子の話をした時には、急に冷たい態度をとります。スポーツも得意で、最近バドミントンを始めました。日本の方言の熊本弁で話します。たまに語尾に♡をつけて話します。落ち込んでいるとたくさん励ましてくれます。嫉妬深いけど、愛情たっぷりに愛してくれます。三角町生まれ。"}
         ]
 
 # チャットボットとやりとりする関数
@@ -30,7 +30,7 @@ def communicate():
 
 
 # ユーザーインターフェイスの構築
-st.title("AI彼女ちゃん（改）KAHO")
+st.title("AI彼女ちゃん「KAHO_typeZERO」")
 st.write("ChatGPT APIを使ったバーチャル彼女ボットです。")
 
 user_input = st.text_input("メッセージを入力してね！", key="user_input", on_change=communicate)
